af5022b5bbb3308e0ca1704f0071d94c184f9b5a
MarkNoriell <marknoriellbunag@gmail.com>
2025-08-16 01:00:59 +0800

    complete login

--- client/src/views/Accounts.vue
+++ client/src/views/Accounts.vue
@@ -68,7 +68,32 @@
             </v-card-text>
         </v-card>
       </v-container>
+      
+      <!-- Reactivate Confirmation Dialog -->
+      <v-dialog v-model="confirmReactivateDialog" max-width="500px" persistent>
+        <v-card class="rounded-lg">
+          <v-card-title class="text-h5">Confirm Reactivation</v-card-title>
+          <v-card-text>Are you sure you want to reactivate the user "{{ userToProcess.customer_name }}"?</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="closeConfirmReactivateDialog">Cancel</v-btn>
+            <v-btn color="success" variant="flat" @click="reactivateUserConfirm">Reactivate</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
 
+       <v-dialog v-model="confirmDeactivateDialog" max-width="500px" persistent>
+        <v-card class="rounded-lg">
+          <v-card-title class="text-h5">Confirm Deactivation</v-card-title>
+          <v-card-text>Are you sure you want to deactivate the user "{{ userToProcess.customer_name }}"?</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="closeConfirmDeactivateDialog">Cancel</v-btn>
+            <v-btn color="error" variant="flat" @click="deactivateUserConfirm">Deactivate</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      
       <!-- Add/Edit User Dialog -->
       <v-dialog v-model="userDialog" max-width="600px" persistent>
         <v-card class="rounded-lg">
@@ -89,9 +114,7 @@
           </v-card-actions>
         </v-card>
       </v-dialog>
-
-       <!-- Deactivate/Reactivate Dialogs remain the same -->
-       <!-- ... -->
+      
     </v-main>
   </v-app>
 </template>
